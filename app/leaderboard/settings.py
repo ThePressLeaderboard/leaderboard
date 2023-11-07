65f58554f15bb2fd428f32d5848aefd85c8375ec
Joe <jihobak0@gmail.com>
2023-11-07 18:54:18 +0900

    프로젝트 코드 품질 및 스타일 검사 셋팅과 코드 커버리지 셋팅
    설치 라이브러리
        - 코드 품질 및 스타일
            - flake8
            - black
            - isort
        - 커버
            - pytest-cov
    
    라이브러리 설정 파일
        - flake8 -> setup.cfg
        - black -> pyproject.toml
        - isort -> .isort.cfg
        - .coveragerc -> pytest-cov
    
    주요 코드 변경사항
    - leaderboard/settings.py : 프로젝트 기본 모드를 디버그 모드로 변경
    - 나머지 파일들은 코드 스타일 검사 통과를 위해서 변경

--- app/leaderboard/settings.py
+++ app/leaderboard/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "KDT")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = int(os.environ.get("DEBUG", default=1))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS",
                                'localhost 127.0.0.1 [::1]'
